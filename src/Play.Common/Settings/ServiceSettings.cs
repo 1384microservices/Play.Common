b16ee51ac3568c4e4b79f1f220d4a0b10ce108be
Andrei Tudor <andrei.tudor@hotmail.com>
2023-03-16 16:49:19 +0200

    Add generic identity settings

--- src/Play.Common/Settings/ServiceSettings.cs
+++ src/Play.Common/Settings/ServiceSettings.cs
@@ -4,4 +4,5 @@ public class ServiceSettings
 {
     public string FQName { get; init; } = string.Empty;
     public string Name { get; init; } = string.Empty;
+    public string Authority { get; init; } = string.Empty;
 }
