0f6a7cc6555e861c18dd2ccff4594fc8c34242c3
Andrei Tudor <andrei.tudor@hotmail.com>
2023-03-30 15:38:22 +0300

    Add mass transit custom retry configuration

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -6,6 +6,8 @@ using MassTransit.Definition;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Play.Common.Configuration;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
+using GreenPipes.Configurators;
 
 namespace Play.Common.MassTransit;
 
@@ -13,23 +15,58 @@ public static class Extensions
 {
     public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services)
     {
-        services.AddMassTransit(cfg =>
+        return services.AddMassTransitWithRabbitMQ(retry => retry.Interval(3, TimeSpan.FromSeconds(3)));
+    }
+
+    public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services, Action<IRetryConfigurator> retryConfigurator)
+    {
+        services.AddMassTransit(busConfigurator =>
         {
-            cfg.AddConsumers(Assembly.GetEntryAssembly());
-            cfg.UsingRabbitMq((ctx, cfg) =>
+            busConfigurator.AddConsumers(Assembly.GetEntryAssembly());
+            busConfigurator.UsingRabbitMq((ctx, cfg) =>
             {
                 var configuration = ctx.GetService<IConfiguration>();
-                cfg.Host(configuration.GetRabbitMQSettings().Host);
-                cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(configuration.GetServiceSettings().Name, false));
-                cfg.UseMessageRetry(retry =>
-                {
-                    retry.Interval(3, TimeSpan.FromSeconds(5));
-                });
+                var rabbitMqConfiguration = configuration.GetRabbitMQSettings();
+                var serviceSettings = configuration.GetServiceSettings();
+                cfg.Host(rabbitMqConfiguration.Host);
+                cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(serviceSettings.Name, false));
+                cfg.UseMessageRetry(retryConfigurator);
             });
         });
-
         services.AddMassTransitHostedService();
-
         return services;
     }
+
+    // private static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator, Action<IRetryConfigurator> retryConfigurator)
+    // {
+    //     serviceCollectionBusConfigurator.UsingRabbitMq((ctx, cfg) =>
+    //     {
+    //         var configuration = ctx.GetService<IConfiguration>();
+    //         var rabbitMqConfiguration = configuration.GetRabbitMQSettings();
+    //         var serviceSettings = configuration.GetServiceSettings();
+    //         cfg.Host(rabbitMqConfiguration.Host);
+    //         cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(serviceSettings.Name, false));
+    //         cfg.UseMessageRetry(retryConfigurator);
+    //     });
+    // }
+
+    // private static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator)
+    // {
+
+    //     serviceCollectionBusConfigurator.UsingPlayEconomyRabbitMQ(retryConfigurator =>
+    //     {
+    //         retryConfigurator.Interval(3, TimeSpan.FromSeconds(3));
+    //     });
+
+    //     serviceCollectionBusConfigurator.UsingRabbitMq((ctx, cfg) =>
+    //     {
+    //         var configuration = ctx.GetService<IConfiguration>();
+    //         cfg.Host(configuration.GetRabbitMQSettings().Host);
+    //         cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(configuration.GetServiceSettings().Name, false));
+    //         cfg.UseMessageRetry(retryCfg =>
+    //         {
+    //             retryCfg.Interval(3, TimeSpan.FromSeconds(3));
+    //         });
+    //     });
+    // }
 }
