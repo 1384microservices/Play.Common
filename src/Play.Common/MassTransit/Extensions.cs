fbf64542de8be412a7e75604099f023698e3c99a
Andrei Tudor <andrei.tudor@hotmail.com>
2023-04-04 20:47:27 +0300

    Cleanup masstransit usage

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -15,7 +15,13 @@ public static class Extensions
 {
     public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services)
     {
-        return services.AddMassTransitWithRabbitMQ(retry => retry.Interval(3, TimeSpan.FromSeconds(3)));
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.AddConsumers(Assembly.GetEntryAssembly());
+            busConfigurator.UsingPlayEconomyRabbitMQ(retry => retry.Interval(3, TimeSpan.FromSeconds(3)));
+        });
+        services.AddMassTransitHostedService();
+        return services;
     }
 
     public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services, Action<IRetryConfigurator> retryConfigurator)
@@ -23,50 +29,28 @@ public static class Extensions
         services.AddMassTransit(busConfigurator =>
         {
             busConfigurator.AddConsumers(Assembly.GetEntryAssembly());
-            busConfigurator.UsingRabbitMq((ctx, cfg) =>
-            {
-                var configuration = ctx.GetService<IConfiguration>();
-                var rabbitMqConfiguration = configuration.GetRabbitMQSettings();
-                var serviceSettings = configuration.GetServiceSettings();
-                cfg.Host(rabbitMqConfiguration.Host);
-                cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(serviceSettings.Name, false));
-                cfg.UseMessageRetry(retryConfigurator);
-            });
+            busConfigurator.UsingPlayEconomyRabbitMQ(retryConfigurator);
         });
         services.AddMassTransitHostedService();
         return services;
     }
 
-    // private static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator, Action<IRetryConfigurator> retryConfigurator)
-    // {
-    //     serviceCollectionBusConfigurator.UsingRabbitMq((ctx, cfg) =>
-    //     {
-    //         var configuration = ctx.GetService<IConfiguration>();
-    //         var rabbitMqConfiguration = configuration.GetRabbitMQSettings();
-    //         var serviceSettings = configuration.GetServiceSettings();
-    //         cfg.Host(rabbitMqConfiguration.Host);
-    //         cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(serviceSettings.Name, false));
-    //         cfg.UseMessageRetry(retryConfigurator);
-    //     });
-    // }
+    public static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator)
+    {
 
-    // private static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator)
-    // {
+        serviceCollectionBusConfigurator.UsingPlayEconomyRabbitMQ(retryConfigurator => retryConfigurator.Interval(3, TimeSpan.FromSeconds(3)));
+    }
 
-    //     serviceCollectionBusConfigurator.UsingPlayEconomyRabbitMQ(retryConfigurator =>
-    //     {
-    //         retryConfigurator.Interval(3, TimeSpan.FromSeconds(3));
-    //     });
-
-    //     serviceCollectionBusConfigurator.UsingRabbitMq((ctx, cfg) =>
-    //     {
-    //         var configuration = ctx.GetService<IConfiguration>();
-    //         cfg.Host(configuration.GetRabbitMQSettings().Host);
-    //         cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(configuration.GetServiceSettings().Name, false));
-    //         cfg.UseMessageRetry(retryCfg =>
-    //         {
-    //             retryCfg.Interval(3, TimeSpan.FromSeconds(3));
-    //         });
-    //     });
-    // }
+    private static void UsingPlayEconomyRabbitMQ(this IServiceCollectionBusConfigurator serviceCollectionBusConfigurator, Action<IRetryConfigurator> retryConfigurator)
+    {
+        serviceCollectionBusConfigurator.UsingRabbitMq((ctx, cfg) =>
+        {
+            var configuration = ctx.GetService<IConfiguration>();
+            var rabbitMqConfiguration = configuration.GetRabbitMQSettings();
+            var serviceSettings = configuration.GetServiceSettings();
+            cfg.Host(rabbitMqConfiguration.Host);
+            cfg.ConfigureEndpoints(ctx, new KebabCaseEndpointNameFormatter(serviceSettings.Name, false));
+            cfg.UseMessageRetry(retryConfigurator);
+        });
+    }
 }
