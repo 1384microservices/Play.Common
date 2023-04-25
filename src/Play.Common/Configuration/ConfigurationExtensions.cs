d839803d46ee97c216367639bda0df5985082acd
Andrei Tudor <andrei.tudor@hotmail.com>
2023-04-25 20:43:36 +0300

    Cleanup

--- src/Play.Common/Configuration/ConfigurationExtensions.cs
+++ src/Play.Common/Configuration/ConfigurationExtensions.cs
@@ -1,29 +1,7 @@
-using System;
-using Azure.Identity;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Play.Common.Settings;
 
 namespace Play.Common.Configuration;
-
-public static class HostBuilderExtensions
-{
-    public static IHostBuilder ConfigureAzureKeyVault(this IHostBuilder hostBuilder)
-    {
-        hostBuilder.ConfigureAppConfiguration((context, configurationBuilder) =>
-        {
-            if (context.HostingEnvironment.IsProduction())
-            {
-                var configuration = configurationBuilder.Build();
-                var serviceSettings = configuration.GetServiceSettings();
-                var uri = new Uri($"https://{serviceSettings.KeyVaultName}.vault.azure.net/");
-                var credentials = new DefaultAzureCredential();
-                configurationBuilder.AddAzureKeyVault(uri, credentials);
-            }
-        });
-        return hostBuilder;
-    }
-}
 
 public static class ConfigurationExtensions
 {
