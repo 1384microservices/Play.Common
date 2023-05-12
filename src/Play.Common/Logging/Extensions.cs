021d45844e1f53f45ff8290225951d867e83099a
Andrei Tudor <andrei.tudor@hotmail.com>
2023-05-12 07:20:27 +0300

    Fix seq settings

--- src/Play.Common/Logging/Extensions.cs
+++ src/Play.Common/Logging/Extensions.cs
@@ -11,7 +11,7 @@ public static class Extensions
     public static IServiceCollection AddSeqLogging(this IServiceCollection services, IConfiguration configuration)
     {
         var seqSettings = configuration.GetSeqSettings();
-        return services.AddLogging(builder => builder.AddSeq(seqSettings.Host));
+        return services.AddLogging(builder => builder.AddSeq(serverUrl:seqSettings.Host));
     }
 
     public static IServiceCollection AddSeqLogging(this IServiceCollection services, SeqSettings seqSettings) {
